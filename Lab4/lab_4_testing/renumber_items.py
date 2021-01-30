03f0d846df14abefc188cc5a2754f6b8d82f144d
David Krug <davidkrug00@gmail.com>
2021-01-29 18:26:32 -0800

    Fixed numbers/python

--- Lab4/lab_4_testing/renumber_items.py
+++ Lab4/lab_4_testing/renumber_items.py
@@ -1,6 +1,6 @@
 # This program will re-number examples entries in a test suite in text
 #   format (before being processed by make_item)
-# All lines that start with "# XX" have the "XX" replaced with the number of "# XX"'s encountered so far. 
+# All lines that start with "# XX" where X is a digit have the "XX" replaced with the number of "# XX"'s encountered so far. 
 
 import sys, copy
 
@@ -24,22 +24,19 @@ lines = file.readlines()
 lines_copy = copy.deepcopy(lines)
 
 # some variables for the loop
-counter = 0 # number of renumbered examples
-example_counter = 0 # number of examples
 line_counter = 0 # number of lines
+example_counter = 0 # number of examples sentences
 
 # for each line
 for line in lines:
-	if line[0:4] == "# XX": # re-number this example
-		counter += 1
-		lines_copy[line_counter] = "# " + str(example_counter) + line[4:]
-	if line[0] == "#": # track the examples seen so far
-		example_counter += 1
+	for i in range(3,7): # check for "# X:", "# XX:", "# XXX:", etc.
+		if line[0] == "#" and line[i]==":": # re-number this example
+			example_counter += 1
+			lines_copy[line_counter] = "# " + str(example_counter) + line[i:]
 	line_counter += 1 # track the lines so far
 
 # print some useful info
-print(str(example_counter) + " examples found.")
-print(str(counter) + " examples renumbered.")
+print(str(example_counter) + " examples found and renumbered.")
 
 # write to the output file
 with open(out_path, 'w') as out_file:
